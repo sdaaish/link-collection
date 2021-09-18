a65a22a2771a2a0557aba67ebd2d964ffc3ace41
Stig Dahl <stig@charlottendal.net>
2021-09-18 19:31:56 +0200

    More testing

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 # Publishes Org files with emacs.
+ls -la
+git submodule update --init
 cp -a org-html-themes/src/ .
+ls -la
 emacs --batch --chdir "${PWD}" --load publish.el --kill
