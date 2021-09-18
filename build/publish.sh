424346c78e83cdb57225b650b0c8ad426d3daf81
sdaaish <stig@charlottendal.net>
2021-09-18 20:38:12 +0000

    Deploying to html from @ sdaaish/link-collection@d366439e09794c69fff5cf3f0f99b57dcc4509a7 🚀

--- build/publish.sh
+++ build/publish.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Publishes Org files with emacs.
-cd ..
+pwd
+ls -la
+
 git submodule update --init
 cp -a org-html-themes/src/ .
 emacs --batch --chdir build --load publish.el --kill
