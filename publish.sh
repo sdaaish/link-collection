4bae6e627429cef682a941249e904122ee97b729
Stig Dahl <stig@charlottendal.net>
2021-09-18 20:14:25 +0200

    Cleaning up the workflow

--- publish.sh
+++ publish.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 # Publishes Org files with emacs.
-ls -la
 git submodule update --init
 cp -a org-html-themes/src/ .
-ls -la
 emacs --batch --chdir "${PWD}" --load publish.el --kill
